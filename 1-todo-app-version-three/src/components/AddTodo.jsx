14d845db2b7187f58785f9a65427b9a3d32231ca
janak bohra <janakbohra29@gmail.com>
2024-01-06 18:45:16 +0530

    form in react

--- 1-todo-app-version-three/src/components/AddTodo.jsx
+++ 1-todo-app-version-three/src/components/AddTodo.jsx
@@ -13,39 +13,38 @@ function AddTodo({ onNewItem }) {
     setDueDate(event.target.value);
   };
 
-  const handleAddButtonClicked = () => {
+  const handleAddButtonClicked = (e) => {
+    e.preventDefault();
     onNewItem(todoName, dueDate);
     setTodoName("");
     setDueDate("");
   };
   return (
-    <div className="row todo-row">
-      <div className="col-6">
-        <input
-          type="text"
-          placeholder="Enter Todo Here"
-          className={styles.todoWidthFull}
-          value={todoName}
-          onChange={handleNameChange}
-        />
-      </div>
-      <div className="col-4">
-        <input
-          type="date"
-          className={styles.todoWidthFull}
-          value={dueDate}
-          onChange={handleDateChange}
-        />
-      </div>
-      <div className="col-2">
-        <button
-          type="button"
-          className="btn btn-success todo-button"
-          onClick={handleAddButtonClicked}
-        >
-          <GrAdd />
-        </button>
-      </div>
+    <div className="container text-center">
+      <form className="row todo-row" onSubmit={handleAddButtonClicked}>
+        <div className="col-6">
+          <input
+            type="text"
+            placeholder="Enter Todo Here"
+            className={styles.todoWidthFull}
+            value={todoName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div className="col-4">
+          <input
+            type="date"
+            className={styles.todoWidthFull}
+            value={dueDate}
+            onChange={handleDateChange}
+          />
+        </div>
+        <div className="col-2">
+          <button className="btn btn-success todo-button">
+            <GrAdd />
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
