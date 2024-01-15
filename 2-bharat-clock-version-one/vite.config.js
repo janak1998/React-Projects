77518f3ffebdf130983c33275287d9278b802283
janak bohra <janakbohra29@gmail.com>
2024-01-15 13:07:07 +0530

    add

--- 2-bharat-clock-version-one/vite.config.js
+++ 2-bharat-clock-version-one/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/React-Projects/tree/main/2-bharat-clock-version-one/",
+});
